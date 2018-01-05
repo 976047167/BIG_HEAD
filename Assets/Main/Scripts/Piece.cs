d40b384683bfb9975c4654394d0512bf72086108
CSF <952327293@qq.com>
2018-01-06 01:09:23 +0800

    初始化
    Signed-off-by: CSF <952327293@qq.com>

---
+++ Assets/Main/Scripts/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Material matOpened;
+    public Material matCurrent;
+    public Material matClosed;
+    public Material matShop;
+    public Material matMonster;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
