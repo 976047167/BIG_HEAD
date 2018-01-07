47d04f873f95b3926bcab1e977c6d9ebf50acaaa
CSF <952327293@qq.com>
2018-01-08 00:08:05 +0800

    卡牌数据结构体

--- Assets/Main/Scripts/Data/MapData.cs
+++ Assets/Main/Scripts/Data/MapData.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapData : MonoBehaviour
+public class MapData
 {
+    private static MapData _instance;
+    public static MapData Instance
+    {
+        get
+        {
+            if (_instance==null)
+            {
+                _instance = new MapData();
+            }
+            return _instance;
+        }
+    }
 
     
+
 }
