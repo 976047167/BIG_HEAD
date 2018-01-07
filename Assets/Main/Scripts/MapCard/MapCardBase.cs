be07278691e3501934748492ae6cf91560333218
CSF <952327293@qq.com>
2018-01-07 20:09:38 +0800

    卡牌代码

---
+++ Assets/Main/Scripts/MapCard/MapCardBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MapCardBase : MonoBehaviour
+{
+    [SerializeField]
+    private Material matOpened;
+    [SerializeField]
+    private Material matCurrent;
+    [SerializeField]
+    private Material matClosed;
+    [SerializeField]
+    private Material matShop;
+    [SerializeField]
+    private Material matMonster;
+    [SerializeField]
+    private Material matClick;
+    public GameObject prefab;
+    public TextMeshPro infoBoard;
+    // Use this for initialization
+    protected virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+
+    }
+
+
+}
---
+++ Assets/Main/Scripts/MapLogic/MapLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLogic : MonoBehaviour
+{
+    public GameObject mapCardTemplate;
+    public List<GameObject> list;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
