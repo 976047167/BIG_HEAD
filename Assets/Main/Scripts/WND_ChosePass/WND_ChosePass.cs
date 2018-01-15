17fe26e54dc29baddadc439b354355336770d633
FH <976047167@qq.com>
2018-01-16 02:50:01 +0800

    UI部分

---
+++ Assets/Main/Scripts/WND_ChosePass/WND_ChosePass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WND_ChosePass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public struct Dialog
+    {
+
+    }
+    void showDialog(string txt)
+    {
+        IEnumerator printChar()
+        {
+
+        }
+    }
+}
