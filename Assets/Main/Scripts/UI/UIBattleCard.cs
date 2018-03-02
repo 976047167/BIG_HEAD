483964ecfac09f7956cc2e9ab6a9501db9142d20
changshanfu@antinc.cn <changshanfu@antinc.cn>
2018-03-02 18:32:25 +0800

    战斗界面

---
+++ Assets/Main/Scripts/UI/UIBattleCard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBattleCard : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnDragStart()
+    {
+
+    }
+    void OnDrag(Vector2 delta)
+    {
+
+    }
+    void OnDragOver()
+    {
+
+    }
+    void OnDragOut()
+    {
+
+    }
+    void OnDragEnd()
+    {
+
+    }
+    void OnDrop(GameObject go)
+    {
+        
+    }
+}
