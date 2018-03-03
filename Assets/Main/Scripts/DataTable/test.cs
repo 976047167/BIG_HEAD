551eb2b2cec0468d7d8e92505ec58fc0180ce088
FH <976047167@qq.com>
2018-03-03 12:14:38 +0800

    运用反射机制的数据读取类，附上test类例子

---
+++ Assets/Main/Scripts/DataTable/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KEngine.Table;
+public class test : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        TextAsset testFile = Resources.Load("1") as TextAsset;
+        print(testFile.text);
+        TableFile<DialogTable1> sss = new TableFile<DialogTable1>(testFile.text);
+        print(sss.GetRowCount());
+        DialogTable1 line = sss.GetRow(1);
+        print(line.text);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+
+    }
+}
