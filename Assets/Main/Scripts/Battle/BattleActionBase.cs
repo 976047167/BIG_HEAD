e9182f13e67dd1a09aebf4d71e95525d23e0df04
CSF <952327293@qq.com>
2018-03-13 03:09:52 +0800

    buff及效果管理类

---
+++ Assets/Main/Scripts/Battle/BattleActionBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 游戏里面的每一种效果
+/// </summary>
+public abstract class BattleActionBase
+{
+    /// <summary>
+    /// 当前action的ID
+    /// </summary>
+    /// <returns></returns>
+    public abstract int GetActionId();
+    /// <summary>
+    /// 效果的实现
+    /// </summary>
+    /// <param name="num"></param>
+    public abstract void GameAction(int num);
+    
+}
---
+++ Assets/Main/Scripts/Battle/BattleActionType.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleActionType : int
+{
+    None = 0,
+    /// <summary>
+    /// 添加一个buff
+    /// </summary>
+    AddBuff = 1,
+    //////////////////////////////////////各种攻击，包括属性攻击100-199
+    /// <summary>
+    /// 普通攻击
+    /// </summary>
+    Attack = 100,
+
+
+
+    /////////////////////////////////////各种回血，200-299
+    /// <summary>
+    /// 回血
+    /// </summary>
+    RecoverHP = 2,
+    /////////////////////////////////////各种回蓝，300-399
+    /// <summary>
+    /// 回蓝
+    /// </summary>
+    RecoverMP = 3,
+    /// <summary>
+    /// 抽卡
+    /// </summary>
+    DrawCard = 4,
+    /// <summary>
+    /// 回血buff
+    /// </summary>
+    BuffRecoverHP,
+    /// <summary>
+    /// 持续伤害buff
+    /// </summary>
+    BuffDamage,
+
+
+
+}
