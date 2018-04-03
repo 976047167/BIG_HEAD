f121978d45d323c9d1a7d36b501b9618cd756d6f
CSF <952327293@qq.com>
2018-04-04 04:29:49 +0800

    AI剥离

--- Assets/Main/Scripts/Battle/BattlePlayer.cs
+++ Assets/Main/Scripts/Battle/BattlePlayer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BattlePlayer
 {
+    public bool UseAI { private set; get; }
+    public BattlePlayerData Data { get; private set; }
+    private BattlePlayerAI playerAI;
     public bool IsMe()
     {
         return false;
