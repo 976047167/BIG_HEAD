df0425df1a0fd05362fcbcc93491555f57aeb8fe
DESKTOP-RKIN7IV\FH <976047167@qq.com>
2018-05-07 01:45:28 +0800

    设置界面UI，与部分代码

---
+++ Assets/Main/Scripts/UI/WND_Settings/WND_Settings.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WND_Settings : UIFormBase {
+
+    // Use this for initialization
+
+    private UISlider sliderVoice;
+    private UISlider sliderMusic;
+    private UISprite spExit;
+    private UISprite spVoice;
+    private UISprite spMusic;
+    private void Awake()
+    {
+        spVoice = transform.Find("bg/frame/spVoice").GetComponent<UISprite>();
+        sliderVoice = spVoice.transform.Find("sliderVoice").GetComponent<UISlider>();
+        spMusic = transform.Find("bg/frame/spMusic").GetComponent<UISprite>();
+        sliderMusic = spVoice.transform.Find("sliderMusic").GetComponent<UISlider>();
+        spExit = transform.Find("bg/frame/spExit").GetComponent<UISprite>();
+
+        UIEventListener.Get(spExit.gameObject).onClick = exitClick;
+        EventDelegate.Add(sliderMusic.onChange, MusicChange);
+        EventDelegate.Add(sliderVoice.onChange, VoiceChange);
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void VoiceChange()
+    {
+
+    }
+    private void MusicChange()
+    {
+
+    }
+    private void exitClick(GameObject obj)
+    {
+        UIModule.Instance.CloseForm<WND_Settings>();
+
+    }
+}
