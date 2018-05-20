232f0bc120134184d58bb2d01e59647284519f0b
DESKTOP-RKIN7IV\FH <976047167@qq.com>
2018-05-21 01:10:58 +0800

    主场景UI

---
+++ Assets/Main/Scripts/UI/WND_Main/WND_Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WND_Main : UIFormBase {
+
+    private UILabel labName;
+
+
+    // Use this for initialization
+    private void Awake()
+    {
+        labName = transform.Find("background/spFrameHead/labName").GetComponent<UILabel>();
+
+
+
+
+    }
+}
