9c7bc3be8c1737ce1183395534388ff2af430e0b
changshanfu@antinc.cn <changshanfu@antinc.cn>
2018-06-06 15:48:54 +0800

    修复毛球编译错误

--- Assets/Main/Scripts/UI/WND_MainTown/WND_MainTown.cs
+++ Assets/Main/Scripts/UI/WND_MainTown/WND_MainTown.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WND_MainTown : UIFormBase {
+public class WND_MainTown : UIFormBase
+{
 
     private UILabel labName;
     private GameObject btnKaKu;
@@ -17,20 +18,20 @@ public class WND_MainTown : UIFormBase {
     {
         labName = transform.Find("background/spFrameHead/labName").GetComponent<UILabel>();
         labLevel = transform.Find("background/spFrameHead/spLevel/labLevel").GetComponent<UILabel>();
-         labCoin = transform.Find("background/spFrameCoin/labCoinNum").GetComponent<UILabel>();
+        labCoin = transform.Find("background/spFrameCoin/labCoinNum").GetComponent<UILabel>();
         labYuanBao = transform.Find("background/spFrameYuanBao/labYuanBaoNum").GetComponent<UILabel>();
-        labVipLevel= transform.Find("background/spFrameHead/spVipLevel/labVipLevel").GetComponent<UILabel>();
+        labVipLevel = transform.Find("background/spFrameHead/spVipLevel/labVipLevel").GetComponent<UILabel>();
         btnKaKu = transform.Find("background/btnKaKu").gameObject;
         btnDungeon = transform.Find("background/btnDungeon").gameObject;
         UIEventListener.Get(btnKaKu).onClick = KakuClick;
-         UIEventListener.Get(btnDungeon).onClick = DungeonClick;
+        UIEventListener.Get(btnDungeon).onClick = DungeonClick;
 
     }
 
     private void Start()
     {
-        labName.text = Game.DataManager.MyPlayerData.Name;
-        labLevel.text = "" + Game.DataManager.MyPlayerData.Level;
+        labName.text = Game.DataManager.MyPlayer.Data.Name;
+        labLevel.text = "" + Game.DataManager.MyPlayer.Data.Level;
         labVipLevel.text = "" + Game.DataManager.AccountData.VipLevel;
         labYuanBao.text = "" + Game.DataManager.AccountData.Diamonds;
         labCoin.text = "" + Game.DataManager.AccountData.Gold;
