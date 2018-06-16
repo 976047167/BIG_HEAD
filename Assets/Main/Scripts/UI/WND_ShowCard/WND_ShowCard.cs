1a8dcc736176cc0ea840ceb243cafa759a33fa32
DESKTOP-RKIN7IV\FH <976047167@qq.com>
2018-06-17 04:25:15 +0800

    showcard重做

--- Assets/Main/Scripts/UI/WND_ShowCard/WND_ShowCard.cs
+++ Assets/Main/Scripts/UI/WND_ShowCard/WND_ShowCard.cs
@@ -8,21 +8,28 @@ public class WND_ShowCard : UIFormBase
     private GameObject btnDestroy;
     private UITexture icon;
     private UILabel labName;
-    private UILabel spending;
-    private UILabel damage;
+    private UILabel spendingNum;
     private UILabel describle;
+    private UISprite spAttack;
+    private UISprite spSkill;
+    private UISprite spItem;
+    private UISprite spEquip;
+
+
     void Awake()
     {
         btnDestroy = transform.Find("btnDestroy").gameObject;
-        icon = transform.Find("CardBg/Icon").GetComponent<UITexture>();
-        labName = transform.Find("CardBg/Name").GetComponent<UILabel>();
-        spending = transform.Find("CardBg/ExpendCount").GetComponent<UILabel>();
-        damage = transform.Find("CardBg/DamageCount").GetComponent<UILabel>();
-        describle = transform.Find("CardBg/Describle").GetComponent<UILabel>();
-        UIEventListener.Get(btnDestroy).onClick = (GameObject a) => {
-            UIModule.Instance.CloseForm<WND_ShowCard>();
+        icon = transform.Find("CardBg/iconFrame/icon").GetComponent<UITexture>();
+        labName = transform.Find("CardBg/labName").GetComponent<UILabel>();
+        spendingNum = transform.Find("CardBg/describeFrame/spSpending/labSpending").GetComponent<UILabel>();
+        describle = transform.Find("CardBg/describeFrame/ScrollView/labDescribe").GetComponent<UILabel>();
+        spAttack = transform.Find("CardBg/describeFrame/spAttack").GetComponent<UISprite>();
+        spSkill = transform.Find("CardBg/describeFrame/spSkill").GetComponent<UISprite>();
+        spItem = transform.Find("CardBg/describeFrame/spItem").GetComponent<UISprite>();
+        spEquip = transform.Find("CardBg/describeFrame/spEquip").GetComponent<UISprite>();
 
-        };
+
+        UIEventListener.Get(btnDestroy).onClick = Exit;
 
     }
     // Use this for initialization
@@ -31,13 +38,34 @@ public class WND_ShowCard : UIFormBase
         base.OnInit(userdata);
         InitCard((int)userdata);
     }
+    /// <summary>
+    /// 加载卡牌
+    /// </summary>
+    /// <param name="id"></param>
     private void InitCard(int id)
     {
         BattleCardTableSetting card =  BattleCardTableSettings.Get(id);
         icon.Load(card.ShowID);
         labName.text = card.Name;
-        spending.text = ""+ card.Spending;
-        describle.text = card.Desc; 
+        spendingNum.text = card.Spending.ToString();
+        describle.text = card.Desc;
+        ShowCardType(card.Type);
+    }
+    private void ShowCardType(int cardType)
+    {
+        spAttack.gameObject.SetActive(cardType == 0);
+        spEquip.gameObject.SetActive(cardType == 1);
+        spSkill.gameObject.SetActive(cardType == 2);
+        spItem.gameObject.SetActive(cardType == 3);
     }
 
+
+    /// <summary>
+    /// 退出
+    /// </summary>
+    /// <param name="obj"></param>
+    private void Exit(GameObject obj)
+    {
+        UIModule.Instance.CloseForm<WND_ShowCard>();
+    }
 }
