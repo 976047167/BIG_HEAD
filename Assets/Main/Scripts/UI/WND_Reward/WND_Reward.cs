9d59e0734dcc951bc30b51bab9ffef274cf767ab
DESKTOP-RKIN7IV\FH <976047167@qq.com>
2018-06-30 19:20:41 +0800

    奖励界面重做

--- Assets/Main/Scripts/UI/WND_Reward/WND_Reward.cs
+++ Assets/Main/Scripts/UI/WND_Reward/WND_Reward.cs
@@ -8,26 +8,45 @@ public class WND_Reward : UIFormBase
     private UILabel labRewardText;
     private GameObject btnCommond;
     private UIGrid grid;
+    private int monsterId;
+    private GameObject frame;
     // Use this for initialization
-    void Awake()
-    {
-        labRewardText = transform.Find("bg/frame/labReward").GetComponent<UILabel>();
-        btnCommond = transform.Find("bg/frame/btnCommond").gameObject;
-        grid = transform.Find("bg/frame/scrollView/Grid").GetComponent<UIGrid>();
-        UIEventListener.Get(btnCommond).onClick = exitClick;
-        // OnInit(1);
-    }
-
     // Update is called once per frame
     protected override void OnInit(object userdata)
     {
+        labRewardText = transform.Find("bg/frame/labReward").GetComponent<UILabel>();
+        btnCommond = transform.Find("bg/frame/btnCommond").gameObject;
+        frame = transform.Find("bg/frame").gameObject;
+        grid = transform.Find("bg/frame/scrollView/Grid").GetComponent<UIGrid>();
+        UIEventListener.Get(btnCommond).onClick = exitClick;
         base.OnInit(userdata);
-        int monsterId = (int)userdata;
+
+
+        monsterId = (int)userdata;
+       
+    }
+
+    protected override void OnOpen()
+    {
+        base.OnOpen();
         BattleMonsterTableSetting monster = BattleMonsterTableSettings.Get(monsterId);
         List<int> rewardList = monster.RewardIds;
 
         int rewardId = rewardList[Random.Range(0, rewardList.Count)];
+        LoadRewardList(rewardId);
+        PlayShowAnime();
 
+    }
+
+    private void PlayShowAnime()
+    {
+        frame.transform.localScale = new Vector3(0.3f,0.3f, 0.3f);
+        TweenScale.Begin(frame, 0.1f, Vector3.one);
+    }
+
+
+    private void LoadRewardList(int rewardId)
+    {
         RewardTableSetting reward = RewardTableSettings.Get(rewardId);
 
 
@@ -104,7 +123,7 @@ public class WND_Reward : UIFormBase
             UIUtility.SetCardTips(item, cardData.Id);
             UITexture icon = item.transform.Find("texReward").GetComponent<UITexture>();
             icon.gameObject.SetActive(true);
-            icon.Load(cardData.IconLeftID);
+            icon.Load(cardData.ShowID);
             item.transform.parent = grid.transform;
             item.transform.localPosition = new Vector3();
             item.transform.localScale = new Vector3(1, 1, 1);
@@ -112,6 +131,8 @@ public class WND_Reward : UIFormBase
         }
         grid.repositionNow = true;
     }
+
+
     private void exitClick(GameObject obj)
     {
         Game.UI.CloseForm<UIBattleForm>();
