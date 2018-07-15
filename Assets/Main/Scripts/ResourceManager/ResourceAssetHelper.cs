c4024b6d88f4c3fe1269b21a33196836b1823cdf
CSF <952327293@qq.com>
2018-07-16 00:29:07 +0800

    正版

---
+++ Assets/Main/Scripts/ResourceManager/ResourceAssetHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceAssetHelper : MonoBehaviour
+{
+    OnAssetDestory destory = null;
+    public void Init(OnAssetDestory destory)
+    {
+        this.destory = destory;
+    }
+    private void OnDestroy()
+    {
+        destory();
+    }
+}
