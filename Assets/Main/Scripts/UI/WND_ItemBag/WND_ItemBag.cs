098c37a6de51c5007bdcf7a4c29228799efdf37b
DESKTOP-RKIN7IV\FH <976047167@qq.com>
2018-08-01 03:05:37 +0800

    背包UI重做

---
+++ Assets/Main/Scripts/UI/WND_ItemBag/WND_ItemBag.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AppSettings;
+
+public class WND_ItemBag : UIFormBase {
+    bool isInLobby;
+    GameObject itemInstence;
+    GameObject bg;
+    UISprite itemBg; 
+    UIGrid grid;
+
+    protected override void OnInit(object userdata)
+    {
+        base.OnInit(userdata);
+        isInLobby = (bool)userdata;
+        bg = transform.Find("bg").gameObject;
+        itemBg = bg.transform.Find("itemBg").GetComponent<UISprite>();
+        itemInstence = bg.transform.Find("itemInstence").gameObject;
+        grid = itemBg.transform.Find("ScrollView/Grid").GetComponent<UIGrid>();
+    }
+    protected override void OnOpen()
+    {
+        base.OnOpen();
+        int num = ItemTableSettings.GetInstance().Count;
+        for(int i = 0; i < num; i++)
+        {
+            GameObject item = Instantiate(itemInstence);
+            item.transform.SetParent(grid.transform);
+            item.transform.localScale = Vector3.one;
+            item.transform.localPosition = Vector3.zero;
+        }
+
+
+    }
+    protected override void OnShow()
+    {
+        base.OnShow();
+        Vector3 orginPos =itemBg.transform.localPosition;
+      //  itemBg.transform.localPosition
+        //TweenPosition.Begin(itemBg.gameObject，0.1f,)
+    }
+
+
+}
