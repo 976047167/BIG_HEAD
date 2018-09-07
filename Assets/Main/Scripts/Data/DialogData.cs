4b9909223ef65dab531a9798b6e064e21008cec9
changshanfu@antinc.cn <changshanfu@antinc.cn>
2018-09-07 18:34:35 +0800

    对话数据结构

---
+++ Assets/Main/Scripts/Data/DialogData.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 一条对话的数据
+/// </summary>
+public class DialogData
+{
+    int head;
+    int showMode;
+    int content;
+    int action;
+    int actionParam;
+
+    public int Head
+    {
+        get
+        {
+            return head;
+        }
+    }
+
+    public int ShowMode
+    {
+        get
+        {
+            return showMode;
+        }
+    }
+
+    public int Content
+    {
+        get
+        {
+            return content;
+        }
+    }
+
+    public int Action
+    {
+        get
+        {
+            return action;
+        }
+    }
+
+    public int ActionParam
+    {
+        get
+        {
+            return actionParam;
+        }
+    }
+    public DialogData(int head, int showMode, int content, int action, int actionParam)
+    {
+        this.head = head;
+        this.showMode = showMode;
+        this.content = content;
+        this.action = action;
+        this.actionParam = actionParam;
+    }
+}
